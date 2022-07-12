54dc26f3b0586371d65256bf5fc65838539545cf
Lauri Kauppinen <ljikaupp@gmail.com>
2022-07-12 15:58:07 +0300

    Do exercises 2.44-2.45

---
+++ 2.2/e2-45.scm
@@ -0,0 +1,33 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define (split op1 op2)
+  (define (proc painter n)
+    (if (= n 0)
+        painter
+        (let ((painter-rec (proc painter (dec n))))
+          (op1 painter
+               (op2 painter-rec painter-rec)))))
+  proc)
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter 
+                                (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right 
+                                   right))
+              (corner (corner-split painter 
+                                    (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right 
+                         corner))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(paint (right-split einstein 5))
+(paint (up-split einstein 5))
+(paint (corner-split einstein 5))
